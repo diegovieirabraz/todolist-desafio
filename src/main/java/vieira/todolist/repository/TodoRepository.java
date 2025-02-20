8c40212dff9a03ea2e9bc43bc8b7708ca108695c
Diego Vieira <diego_vieira141@hotmail.com>
2025-02-19 22:28:56 -0300

    feat: adiciona repositório JPA para entidade Todo

--- src/main/java/vieira/todolist/repository/TodoRepository.java
+++ src/main/java/vieira/todolist/repository/TodoRepository.java
@@ -1,6 +1,7 @@
 package vieira.todolist.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import vieira.todolist.model.Todo;
 
-public interface TodoRepository extends JpaRepository {
+public interface TodoRepository extends JpaRepository<Todo, Long> {
 }
